ca3f26f643e9a78093c7b7f1697a84a66176b7a7
maydaycha <maydaychaaaa@gmail.com>
2015-01-09 19:29:47 +0800

    reset rule engine params

---
+++ src/java/com/intel/formosa/test/Parameters.java
@@ -0,0 +1,10 @@
+package com.intel.formosa.test;
+
+
+public class Parameters {
+    public String[] need_sensor = new String[20];
+    public String[] have_sensor = new String[20];
+    public String compare = "";
+    public Boolean Alarm = false;
+    public Boolean five_s_alive = true;
+}
